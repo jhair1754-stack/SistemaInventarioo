ba7592d09602dd70c7946b2bf70cb74477f14433
Jhairwa <taywa1707@gmail.com>
2026-05-17 22:02:42 -0500

    Prueba_1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("ioioioioio");
+        System.out.printf("ioioioiosadio");
         for (int i = 1; i <= 5; i++) {
 
             System.out.println("i = " + i);
