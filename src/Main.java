a24416ee0530daafe7965c490a30f8d1bffe7905
J.B.J.T. <jack.jimenes@unmsm.edu.pe>
2026-05-17 22:04:17 -0500

    Use netbeans wazaaa

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 14; i++) {
 
             System.out.println("i = " + i);
         }
